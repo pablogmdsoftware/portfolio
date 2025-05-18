bb1c54fa731cd9a43ce1c0918fd4ddd538c10aa0
PabloGMD <pablogmdsoftware@gmail.com>
2025-05-18 20:53:15 +0200

    refactor: Remove "projects/" from url paths

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="root"),
     path("home/", views.home, name="home"),
-    path("projects/data-analysis/", views.data_analysis, name="data_analysis"),
-    path("projects/data-science/", views.data_science, name="data_science"),
-    path("projects/web-development/", views.web_development, name="web_development"),
+    path("data-analysis/", views.data_analysis, name="data_analysis"),
+    path("data-science/", views.data_science, name="data_science"),
+    path("web-development/", views.web_development, name="web_development"),
 ]

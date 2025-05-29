adf1256e99f1f67129ca30c8e766cbe47ca3424d
PabloGMD <pablogmdsoftware@gmail.com>
2025-05-29 18:20:45 +0200

    fix: Correct bus_system url path

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("data-analysis/", views.data_analysis, name="data_analysis"),
     path("data-science/", views.data_science, name="data_science"),
     path("web-development/", views.web_development, name="web_development"),
-    path("bus_system/", views.bus_system, name="bus_system"),
+    path("web-development/bus_system/", views.bus_system, name="bus_system"),
 ]
